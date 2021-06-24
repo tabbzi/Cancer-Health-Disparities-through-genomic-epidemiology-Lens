ae850f599ac67f393438c3884415c19fc7b7f0ec
tabbzi <kathleen.m.morrill@gmail.com>
2021-06-24 13:20:16 +0000

    Script for fetching data based on patient barcodes selcted by ancestry

--- bin/TCGA_FetchFPKMMatrix.R
+++ bin/TCGA_FetchFPKMMatrix.R
@@ -3,6 +3,7 @@ require(tidyverse)
 require(readr)
 require(data.table)
 require(TCGAbiolinks)
+library(biomaRt)
 
 # Cancer Type
 typeCancer = "COAD"
@@ -15,58 +16,43 @@ ancestryTCGA = read.delim(file = "./data/anc/TCGA_consensus_ancestry.tsv",
 
 
 # Query
-query.exp.TCGA.anc.AFR <- GDCquery(project = typeTCGA,
-                                   legacy = F,
-                                   data.category = "Transcriptome Profiling",
-                                   data.type = "Gene Expression Quantification",
-                                   workflow.type = "HTSeq - Counts",                  
-                                   barcode = ancestryTCGA[consensus_ancestry %in% c("afr_admix","afr")],
-                                   sample.type = c("Solid Tissue Normal", "Primary Tumor"))
+query.exp.TCGA.anc.fpkm <- GDCquery(project = typeTCGA,
+                                    legacy = F,
+                                    data.category = "Transcriptome Profiling",
+                                    data.type = "Gene Expression Quantification",
+                                    workflow.type = "HTSeq - FPKM",                  
+                                    sample.type = c("Solid Tissue Normal", "Primary Tumor"))
 
-query.exp.TCGA.anc.EUR <- GDCquery(project = typeTCGA,
-                                   legacy = F,
-                                   data.category = "Transcriptome Profiling",
-                                   data.type = "Gene Expression Quantification",
-                                   workflow.type = "HTSeq - Counts",                  
-                                   barcode = ancestryTCGA[consensus_ancestry %in% c("eur_admix","eur")],
-                                   sample.type = c("Solid Tissue Normal", "Primary Tumor"))
-
-query.exp.TCGA.anc.AFR.fpkm <- GDCquery(project = typeTCGA,
-                                   legacy = F,
-                                   data.category = "Transcriptome Profiling",
-                                   data.type = "Gene Expression Quantification",
-                                   workflow.type = "HTSeq - FPKM",                  
-                                   barcode = ancestryTCGA[consensus_ancestry %in% c("afr_admix","afr")],
-                                   sample.type = c("Solid Tissue Normal", "Primary Tumor"))
-
-query.exp.TCGA.anc.EUR.fpkm <- GDCquery(project = typeTCGA,
-                                   legacy = F,
-                                   data.category = "Transcriptome Profiling",
-                                   data.type = "Gene Expression Quantification",
-                                   workflow.type = "HTSeq - FPKM",                  
-                                   barcode = ancestryTCGA[consensus_ancestry %in% c("eur_admix","eur")],
-                                   sample.type = c("Solid Tissue Normal", "Primary Tumor"))
+GDCdownload(query.exp.TCGA.anc.fpkm)
+exp.TCGA.anc.fpkm <- GDCprepare(query.exp.TCGA.anc.fpkm)
 
 
-GDCdownload(query.exp.TCGA.anc.AFR.fpkm)
-exp.TCGA.anc.AFR.fpkm <- GDCprepare(query.exp.TCGA.anc.AFR.fpkm)
-
-GDCdownload(query.exp.TCGA.anc.EUR.fpkm)
-exp.TCGA.anc.EUR.fpkm <- GDCprepare(query.exp.TCGA.anc.EUR.fpkm)
+# ancestry all
+AFR = ancestryTCGA[tumor_type == typeCancer & consensus_ancestry %in% c("afr_admix","afr")]$patient
+EUR = ancestryTCGA[tumor_type == typeCancer & consensus_ancestry %in% c("eur_admix","eur")]$patient
 
 # why is the sample count the same? they aren't the same samples
-colnames(exp.TCGA.anc.AFR.fpkm) %in% ancestryTCGA[consensus_ancestry %in% c("afr_admix","afr")]$patient %>% length()
-colnames(exp.TCGA.anc.AFR.fpkm) %in% ancestryTCGA[consensus_ancestry %in% c("eur_admix","eur")]$patient %>% length()
+colnames(exp.TCGA.anc.fpkm) %like% "TCGA-QG-A5Z1"
 
 # ensembl to hugo
-library(biomaRt)
 mart = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+annot = getBM(attributes = "hgnc_symbol", "ensembl_gene_id", values = rownames(exp.TCGA.anc.fpkm), mart = mart)
+rownames(exp.TCGA.anc.fpkm) = annot$hgnc_symbol
 
-exp.TCGA.anc.AFR.fpkm.ann <- getBM(attributes = "hgnc_symbol", "ensembl_gene_id", values = rownames(exp.TCGA.anc.AFR.fpkm), mart = mart)
-exp.TCGA.anc.EUR.fpkm.ann <- getBM(attributes = "hgnc_symbol", "ensembl_gene_id", values = rownames(exp.TCGA.anc.EUR.fpkm), mart = mart)
+FPKM = as.data.table(exp.TCGA.anc.fpkm@assays@data)
+FPKM[, group := NULL]
+FPKM[, group_name := NULL]
+colnames(FPKM) = colnames(exp.TCGA.anc.fpkm)
+FPKM$gene = rownames(exp.TCGA.anc.fpkm)
+FPKM = as.data.table(FPKM)
+colstotake = c("gene",colnames(FPKM)[colnames(FPKM)!="gene"])
+write.table(x = FPKM[, colstotake, with = F], file = "../scratch/FPKM/exp.TCGA.anc.ALLGROUPS.fpkm.txt", sep = "\t", row.names = F, col.names = T, quote = F)
 
-rownames(exp.TCGA.anc.AFR.fpkm) = exp.TCGA.anc.AFR.fpkm.ann$hgnc_symbol
-rownames(exp.TCGA.anc.EUR.fpkm) = exp.TCGA.anc.EUR.fpkm.ann$hgnc_symbol
+AFRcols = c("gene",unlist(sapply(AFR, FUN = function(x) colnames(FPKM)[colnames(FPKM) %like% x])))
+EURcols = c("gene",unlist(sapply(EUR, FUN = function(x) colnames(FPKM)[colnames(FPKM) %like% x])))
 
-write.table(x = exp.TCGA.anc.AFR.fpkm@assays@data@listData, file = "../scratch/Cancer-Health-Disparities-through-genomic-epidemiology-Lens/data/exp.TCGA.anc.AFR.fpkm.txt", sep = "\t", row.names = rownames(exp.TCGA.anc.AFR.fpkm), col.names = colnames(exp.TCGA.anc.AFR.fpkm), quote = F)
-write.table(x = exp.TCGA.anc.EUR.fpkm@assays@data@listData, file = "../scratch/Cancer-Health-Disparities-through-genomic-epidemiology-Lens/data/exp.TCGA.anc.EUR.fpkm.txt", sep = "\t", row.names = rownames(exp.TCGA.anc.EUR.fpkm), col.names = colnames(exp.TCGA.anc.EUR.fpkm), quote = F)
+EURcols_samp67 = c("gene",sample(unlist(sapply(EUR, FUN = function(x) colnames(FPKM)[colnames(FPKM) %like% x])), size = 67, replace = FALSE))
+
+write.table(x = FPKM[, AFRcols, with = F], file = "../scratch/FPKM/exp.TCGA.anc.AFR.fpkm.txt", sep = "\t", row.names = F, col.names = T, quote = F)
+write.table(x = FPKM[, EURcols, with = F], file = "../scratch/FPKM/exp.TCGA.anc.EUR.fpkm.txt", sep = "\t", row.names = F, col.names = T, quote = F)
+write.table(x = FPKM[, EURcols_samp67, with = F], file = "../scratch/FPKM/exp.TCGA.anc.EUR.random.fpkm.txt", sep = "\t", row.names = F, col.names = T, quote = F)
